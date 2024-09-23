a092b61b85f3f4c1d733d340dd3ac8499b8c75d9
n6ck <n@ermm.rest>
2024-09-22 18:16:24 -0700

    Landed.

--- cobolt/enums.py
+++ cobolt/enums.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-from enum import Enum, auto
+from enum import Enum
 
 __all__ = ("Status",)
 
 
-class Status(Enum):
-    ERROR = auto()
-    PICKER = auto()
-    REDIRECT = auto()
-    TUNNEL = auto()
+class Status(str, Enum):
+    ERROR = "error"
+    PICKER = "picker"
+    REDIRECT = "redirect"
+    TUNNEL = "tunnel"
